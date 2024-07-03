62382f5bd6a26bdea826e9ea17fa9e07faa2b74c
yuenze <yuenzejack@gmail.com>
2024-07-03 16:37:57 +0700

    python file

--- python_dot_product.py
+++ python_dot_product.py
@@ -15,10 +15,6 @@ def timer(func):
     return wrapper
 
 # 1D array dot product
-def generate_1d_array(size, range_):
-    arr = [rn.randrange(*range_) for _ in range(size)]
-    return arr
-
 @timer
 def py_1d_dot(arr1, arr2):
     return sum(x * y for x, y in zip(arr1, arr2))
@@ -52,7 +48,7 @@ def py_2d_dot(arr1, arr2):
 
 @timer
 def np_2d_dot(arr1, arr2):
-    return np.matmul(arr1, arr2)
+    return np.array(arr1).dot(arr2)
 
 @timer
 def rust_2d_dot(arr1, arr2):
@@ -78,42 +74,39 @@ if __name__ == '__main__':
     # print(f"dot product time taken with python: {python_time_taken:.6f} seconds, result is {python_result}")
     # print(f"dot product time taken with numpy: {numpy_time_taken:.6f} seconds, result is {numpy_result}")
     # Generate large random float arrays
-    arr1 = np.random.rand(50000000)
-    arr2 = np.random.rand(50000000)
-
-    # Convert NumPy arrays to lists
-    start_time = time.time()
-    arr1_list = arr1.tolist()
-    arr2_list = arr2.tolist()
-    end_time = time.time()
-    conversion_time = end_time - start_time
-    print(f"Time taken for conversion to list: {conversion_time:.6f} seconds")
-
-    # Benchmark the function that works with Vec<f64>
-    rust_time_taken, rust_result = rust_1d_dot(arr1_list, arr2_list)
-    print(f"dot product time taken with rust (Vec<f64>): {rust_time_taken:.6f} seconds, result is {rust_result}")
-
-    # Benchmark the function that works with PyAny (directly with NumPy arrays)
-    rust_any_time_taken, rust_any_result = rust_1d_dot_any(arr1, arr2)
-    print(f"dot product time taken with rust any: {rust_any_time_taken:.6f} seconds, result is {rust_any_result}")
-
-    # Benchmark the function in Python
-    python_time_taken, python_result = py_1d_dot(arr1, arr2)
-    print(f"dot product time taken with python: {python_time_taken:.6f} seconds, result is {python_result}")
-
-    # Benchmark the function in NumPy
-    numpy_time_taken, numpy_result = np_1d_dot(arr1, arr2)
-    print(f"dot product time taken with numpy: {numpy_time_taken:.6f} seconds, result is {numpy_result}")
+    # ..................................................................
+    # arr1 = np.random.randint(10000, size=50000000)
+    # arr2 = np.random.randint(10000, size=50000000)
+    #
+    # # Convert NumPy arrays to lists
+    # start_time = time.time()
+    # arr1_list = arr1.tolist()
+    # arr2_list = arr2.tolist()
+    # end_time = time.time()
+    # conversion_time = end_time - start_time
+    # print(f"Time taken for conversion to list: {conversion_time:.6f} seconds")
+    #
+    # # Benchmark the function that works with Vec<f64>
+    # rust_time_taken, rust_result = rust_1d_dot(arr1_list, arr2_list)
+    # print(f"dot product time taken with rust (Vec<i64>): {rust_time_taken:.6f} seconds, result is {rust_result}")
+    #
+    # # Benchmark the function that works with PyAny (directly with NumPy arrays)
+    # rust_any_time_taken, rust_any_result = rust_1d_dot_any(arr1, arr2)
+    # print(f"dot product time taken with rust any: {rust_any_time_taken:.6f} seconds, result is {rust_any_result}")
+    #
+    # # Benchmark the function in Python
+    # python_time_taken, python_result = py_1d_dot(arr1, arr2)
+    # print(f"dot product time taken with python: {python_time_taken:.6f} seconds, result is {python_result}")
+    #
+    # # Benchmark the function in NumPy
+    # numpy_time_taken, numpy_result = np_1d_dot(arr1, arr2)
+    # print(f"dot product time taken with numpy: {numpy_time_taken:.6f} seconds, result is {numpy_result}")
 
 
     # Benchmarking matrix multiplication
-    # data = generate_2d_array(size=500, range_=(1, 100))
-    #
-    # numpy_time_taken, numpy_result = np_2d_dot(*data)
-    # python_time_taken, python_result = py_2d_dot(*data)
-    # rust_time_taken, rust_result = rust_2d_dot(*data)
-    #
-    # print(f"matrix multiplication time taken with numpy: {numpy_time_taken} seconds")
-    # print(f"matrix multiplication time taken with python: {python_time_taken} seconds")
-    # print(f"matrix multiplication time taken with rust: {rust_time_taken} seconds")
+    data = generate_2d_array(size=500, range_=(1, 100))
+    numpy_time_taken, numpy_result = np_2d_dot(*data)
+    python_time_taken, python_result = py_2d_dot(*data)
+    print(f"time taken with numpy: {numpy_time_taken} seconds")
+    print(f"time taken with python: {python_time_taken} seconds")
 
