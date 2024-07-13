264fe50e94dd45a7440706f2664334508535d65a
yuenze <yuenzejack@gmail.com>
2024-07-13 20:20:28 +0700

    benchmarked dot, matmul, argsort

--- python_dot_product.py
+++ python_dot_product.py
@@ -2,7 +2,7 @@ import time
 import numpy as np
 import random as rn
 import dotpro
-
+import c_extension
 
 # Timer Function
 def timer(func):
@@ -14,31 +14,26 @@ def timer(func):
 
     return wrapper
 
-# 1D array dot product
+# dot product
 @timer
-def py_1d_dot(arr1, arr2):
+def py_dot(arr1, arr2):
     return sum(x * y for x, y in zip(arr1, arr2))
 
 @timer
-def np_1d_dot(arr1, arr2):
+def np_dot(arr1, arr2):
     return np.dot(arr1, arr2)
 
 @timer
-def rust_1d_dot(arr1, arr2):
-    return dotpro.dot_product(arr1, arr2)
+def rust_dot(arr1, arr2):
+    return dotpro.dot_product_par_simd(arr1, arr2)
 
-@timer
-def rust_1d_dot_any(arr1, arr2):
-    return dotpro.dot_product_any(arr1, arr2)
-
-# 2D matrix dot product
-
-def generate_2d_array(size, range_):
+# matrix multiplication
+def generate_matrix(size, range_):
     arr = [[[rn.randrange(*range_) for _ in range(size)] for _ in range(size)] for _ in range(2)]
     return arr
 
 @timer
-def py_2d_dot(arr1, arr2):
+def py_matmul(arr1, arr2):
     result = [[0 for _ in range(len(arr1))] for _ in range(len(arr2[0]))]
     for i in range(len(arr1)):
         for j in range(len(arr2[0])):
@@ -47,66 +42,72 @@ def py_2d_dot(arr1, arr2):
     return result
 
 @timer
-def np_2d_dot(arr1, arr2):
+def np_matmul(arr1, arr2):
     return np.array(arr1).dot(arr2)
 
 @timer
-def rust_2d_dot(arr1, arr2):
-    return dotpro.matrix_multiply(arr1, arr2)
+def c_matmul(arr1, arr2):
+    return c_extension.dot_product_optimized_parallel(arr1, arr2)
+
+# Argsort
+@timer
+def rust_argsort(arr):
+    return dotpro.argsort(arr)
+
+@timer
+def np_argsort(arr):
+    return np.argsort(arr)
+
 
 if __name__ == '__main__':
 
-    # Benchmarking dot product of 1D array
-    # arr1 = generate_1d_array(700000, (1, 100))
-    # arr2 = generate_1d_array(700000, (1, 100))
-    # arr1 = np.random.rand(50000000)
-    # arr2 = np.random.rand(50000000)
-    # arr1_list = arr1.tolist()
-    # arr2_list = arr2.tolist()
+    # Benchmarking dot product
+    arr1 = np.random.rand(50000000)
+    arr2 = np.random.rand(50000000)
+
+    numpy_time_taken, numpy_result = np_dot(arr1, arr2)
+    print(f"dot product time taken with numpy: {numpy_time_taken:.6f} seconds, result is {numpy_result}")
+
+    simd_time_taken, simd_result = rust_dot(arr1, arr2)
+    print(f"dot product time taken with rust: {simd_time_taken:.6f} seconds, result is {simd_result}")
+
+    # def benchmark_implementations():
     #
-    # numpy_time_taken, numpy_result = np_1d_dot(arr1, arr2)
-    # python_time_taken, python_result = py_1d_dot(arr1, arr2)
-    # rust_time_taken, rust_result = rust_1d_dot(arr1_list, arr2_list)
-    # rust_any_time_taken, rust_any_result = rust_1d_dot_any(arr1, arr2)
+    #     total_np_time = 0.0
+    #     total_rust_time = 0.0
     #
-    # print(f"dot product time taken with rust: {rust_time_taken:.6f} seconds, result is {rust_result}")
-    # print(f"dot product time taken with rust any: {rust_any_time_taken:.6f} seconds, result is {rust_any_result}")
-    # print(f"dot product time taken with python: {python_time_taken:.6f} seconds, result is {python_result}")
-    # print(f"dot product time taken with numpy: {numpy_time_taken:.6f} seconds, result is {numpy_result}")
-    # Generate large random float arrays
-    # ..................................................................
-    # arr1 = np.random.randint(10000, size=50000000)
-    # arr2 = np.random.randint(10000, size=50000000)
+    #     for _ in range(50):
+    #         arr_x = np.random.rand(50000000)
+    #         arr_y = np.random.rand(50000000)
     #
-    # # Convert NumPy arrays to lists
-    # start_time = time.time()
-    # arr1_list = arr1.tolist()
-    # arr2_list = arr2.tolist()
-    # end_time = time.time()
-    # conversion_time = end_time - start_time
-    # print(f"Time taken for conversion to list: {conversion_time:.6f} seconds")
+    #         np_time, numpy_result = np_dot(arr_x, arr_y)
+    #         total_np_time += np_time
     #
-    # # Benchmark the function that works with Vec<f64>
-    # rust_time_taken, rust_result = rust_1d_dot(arr1_list, arr2_list)
-    # print(f"dot product time taken with rust (Vec<i64>): {rust_time_taken:.6f} seconds, result is {rust_result}")
+    #         rust_time, simd_result = rust_dot(arr_x, arr_y)
+    #         total_rust_time += rust_time
     #
-    # # Benchmark the function that works with PyAny (directly with NumPy arrays)
-    # rust_any_time_taken, rust_any_result = rust_1d_dot_any(arr1, arr2)
-    # print(f"dot product time taken with rust any: {rust_any_time_taken:.6f} seconds, result is {rust_any_result}")
+    #     print(f"Total dot product time taken with NumPy over 50 runs: {total_np_time:.6f} seconds, average time is {total_np_time/50}")
+    #     print(f"Total dot product time taken with Rust parallel SIMD over 50 runs: {total_rust_time:.6f} seconds, average time is {total_rust_time/50}")
     #
-    # # Benchmark the function in Python
-    # python_time_taken, python_result = py_1d_dot(arr1, arr2)
-    # print(f"dot product time taken with python: {python_time_taken:.6f} seconds, result is {python_result}")
-    #
-    # # Benchmark the function in NumPy
-    # numpy_time_taken, numpy_result = np_1d_dot(arr1, arr2)
-    # print(f"dot product time taken with numpy: {numpy_time_taken:.6f} seconds, result is {numpy_result}")
+    # benchmark_implementations()
+
 
 
     # Benchmarking matrix multiplication
-    data = generate_2d_array(size=500, range_=(1, 100))
-    numpy_time_taken, numpy_result = np_2d_dot(*data)
-    python_time_taken, python_result = py_2d_dot(*data)
-    print(f"time taken with numpy: {numpy_time_taken} seconds")
-    print(f"time taken with python: {python_time_taken} seconds")
+    data = generate_matrix(size=800, range_=(1, 100))
+    numpy_time_taken, numpy_result = np_matmul(*data)
+    # python_time_taken, python_result = py_matmul(*data)
+    c_par_time_taken, c_result = c_matmul(*data)
+    # print(f"matrix multiply time taken with python: {python_time_taken} seconds")
+    print(f"matrix multiply time taken with numpy: {numpy_time_taken} seconds")
+    print(f"matrix multiply time taken with c par: {c_par_time_taken} seconds")
+
+    # Benchmarking argsort
+    data = np.random.randint(0, 100000, size=5000000).tolist()
+
+    np_time_taken, mp_result = np_argsort(data)
+    print(f"argsort time taken with numpy: {np_time_taken:.6f} seconds")
+    rust_time_taken, rust_result = rust_argsort(data)
+    print(f"argsort time taken with rust: {rust_time_taken:.6f} seconds")
+
 
